351103797afcfa39e98a4bddb7bee7acd350d365
boonditoons <boonditoons@gmail.com>
2026-06-18 09:45:30 +0000

    data types

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newew version
+
+//alert (3 + 4) /
+let name = "ashish" //syring data types
+let age = 18 //int data types
+let isLooggedIn = false // boolen data types
+
+
+//number =. 2 to the power  53//
+//null => standalone value
+//undified => value not assigned yet
+//symbol => unique
+
+// objject 
+
+
+console.log(typeof age );
+console.log(typeof null );
+console.log(typeof string );
+console.log(typeof age );
