89920130c9e70b75568bd18d182af7f92d02ebba
boonditoons <boonditoons@gmail.com>
2026-06-25 08:54:28 +0000

    save it

---
+++ basics/string.js
@@ -0,0 +1,58 @@
+//1. variable in java script 
+//let
+//used when the value can change 
+//ex;-
+let name = "ashish";
+console.log(name);
+ 
+name = "ashu";
+console.log(name);
+
+//CONST
+//Used when the value should not be reassignie\
+const country = "India";
+console.log(country);  // country = "USA"; // Error
+  
+//STRING
+
+//A string is tex enclosed in quotes
+// Ex:-
+let str1 = "ashu";
+let str2 = "nbobby"
+let str3 = "singh"
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+//STRING LENGTH
+
+let text = "javascript";
+console.log(text.length); //outpout should be 10
+
+// Access characters
+let text = "Ashish"
+
+console.log(text[0]);
+console.log(text[1]); // out put should be (A,s) 
+
+// String methods 
+// ToUppercase()
+//convert to uppercase
+
+let text = "hello";
+console.log(text.toUppercase); // output should be (HELLO)
+
+//toLowerCase()
+ //convert to lower case
+  let text = "HELLO BABY"
+  console.log(text.toLowerCase);// output should be (hello baby)
+  
+
+
+
+
+
+
+
+
+
